d486081f740cdbafc6a397022a3b12a8c92c2e6e
Adam Bavosa <adam.bavosa@gmail.com>
2020-07-16 18:27:33 -0700

    fleshed out creating an instance with a provider

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,31 @@
+import { ethers } from 'ethers';
 import * as eth from './eth';
+import * as util from './util';
+import * as cToken from './cToken';
+import { constants } from './constants';
 
-const Factory = {
-  Compound: class {
-    constructor() {}
-  },
-  eth,
+const Compound = function(provider: any='mainnet', options: any={}) {
+  options.provider = provider || options.provider;
+  provider = eth.createProvider(options);
+
+  const instance: any = {
+    _ethers: ethers,
+    _provider: provider,
+    ...cToken
+  };
+
+  // Instance needs to know which network the provider connects to, so it can
+  //     use the correct contract addresses.
+  instance._networkPromise = eth.getProviderNetwork(provider).then((network) => {
+    delete instance._networkPromise;
+    instance._network = network;
+  });
+
+  return instance;
 };
 
-export = Factory;
+Compound.eth = eth;
+Compound.util = util;
+Object.assign(Compound, constants);
+
+export = Compound;
