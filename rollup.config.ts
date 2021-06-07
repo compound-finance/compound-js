759f06149c7dea3bee04494cbef721688fe49ad7
ajb413 <adam.bavosa@gmail.com>
2021-06-06 21:34:10 -0700

    remove deprecated build dependency

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,7 @@
 // Rollup builds only the browser version using the Node.js build.
 import { nodeResolve as resolve } from '@rollup/plugin-node-resolve';
 import commonjs from 'rollup-plugin-commonjs';
-import minify from 'rollup-plugin-babel-minify';
+import { terser } from 'rollup-plugin-terser';
 import json from '@rollup/plugin-json';
 
 const BrowserBuildPath = './dist/browser/compound.min.js';
@@ -31,7 +31,7 @@ export default [{
     commonjs({
       namedExports: { Compound: ['Compound'] },
     }),
-    minify({ comments: false }),
+    terser(),
     json(),
   ],
   external: [
